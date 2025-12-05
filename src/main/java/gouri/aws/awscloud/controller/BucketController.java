5a699365f066f8166c774d8f7695a5aa05641c83
Dilip9 <dilip.verma940805@gmail.com>
2025-12-05 23:51:57 +0530

    Swagger implementation

--- src/main/java/gouri/aws/awscloud/controller/BucketController.java
+++ src/main/java/gouri/aws/awscloud/controller/BucketController.java
@@ -2,7 +2,6 @@ package gouri.aws.awscloud.controller;
 
 import gouri.aws.awscloud.service.BucketService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -34,7 +33,7 @@ public class BucketController {
     @Operation(summary = "Create bucket", description = "Create a new S3 bucket")
     @ApiResponses({
             @ApiResponse(responseCode = "201", description = "Bucket created"),
-            @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content)
+            @ApiResponse(responseCode = "400", description = "Invalid input", content = @io.swagger.v3.oas.annotations.media.Content)
     })
     @PostMapping("/add/{bucketName}")
     public ResponseEntity<String> createBucket(@PathVariable String bucketName){
@@ -49,17 +48,32 @@ public class BucketController {
         return new ResponseEntity<>(service.uploadFile(file, bucketName),HttpStatus.OK);
     }
 
+    @Operation(summary = "List files based on buckets", description = "Returns a list of files")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "List returned"),
+            @ApiResponse(responseCode = "500", description = "Server error", content = @io.swagger.v3.oas.annotations.media.Content)
+    })
     @DeleteMapping(path="/delete/file/{bucketName}/{fileName}")
     public ResponseEntity<String> deleteFile(@PathVariable String bucketName,@PathVariable String fileName)
     {
         return new ResponseEntity<>(service.deleteFile(bucketName,fileName),HttpStatus.OK);
     }
 
+    @Operation(summary = "Delete bucket", description = "Delete an existing bucket")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "Deleted"),
+            @ApiResponse(responseCode = "404", description = "Not found", content = @io.swagger.v3.oas.annotations.media.Content)
+    })
     @DeleteMapping("/delete/bucket/{bucketName}")
     public ResponseEntity<String> deleteBucket(@PathVariable String bucketName) {
         return new ResponseEntity<>(service.deleteBucket(bucketName), HttpStatus.OK);
     }
 
+    @Operation(summary = "List buckets", description = "Returns a list of buckets")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "List returned"),
+            @ApiResponse(responseCode = "500", description = "Server error", content = @io.swagger.v3.oas.annotations.media.Content)
+    })
     @GetMapping("/show/buckets")
     public ResponseEntity<Map<String, List<String>>> showAllBuckets(){
         return new ResponseEntity<>(service.listBuckets(),HttpStatus.OK);
