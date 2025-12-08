19a88d00a2a0729bd4b478ac964d48adc78a56b5
Dilip9 <dilip.verma940805@gmail.com>
2025-12-08 23:44:54 +0530

    Project Mapper Interface

--- src/main/java/gouri/aws/awscloud/mapper/ProjectMapper.java
+++ src/main/java/gouri/aws/awscloud/mapper/ProjectMapper.java
@@ -1,4 +1,15 @@
 package gouri.aws.awscloud.mapper;
 
-public class ProjectMapper {
+
+@Mapper(componentModel = "spring")
+public interface ProjectMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "resume", ignore = true)
+    Project toEntity(ProjectCreateRequest request);
+
+    ProjectDTO toDTO(Project project);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateProject(ProjectUpdateRequest request, @MappingTarget Project entity);
 }
