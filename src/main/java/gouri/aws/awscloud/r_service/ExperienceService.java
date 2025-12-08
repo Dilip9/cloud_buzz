feaacdfbbace07cbbb29148bd221005fcc6ccae7
Dilip9 <dilip.verma940805@gmail.com>
2025-12-09 00:38:00 +0530

    Resume & Experience Service layer interface creation

--- src/main/java/gouri/aws/awscloud/r_service/ExperienceService.java
+++ src/main/java/gouri/aws/awscloud/r_service/ExperienceService.java
@@ -1,4 +1,11 @@
 package gouri.aws.awscloud.r_service;
 
+import gouri.aws.awscloud.model.dto.request.ExperienceCreateRequest;
+import gouri.aws.awscloud.model.dto.response.ExperienceDto;
+import gouri.aws.awscloud.model.dto.update.ExperienceUpdateRequest;
+
 public interface ExperienceService {
+    ExperienceDto add(Long resumeId, ExperienceCreateRequest request);
+    ExperienceDto update(Long id, ExperienceUpdateRequest request);
+    void delete(Long id);
 }
