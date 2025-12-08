f193cd1269a3c02bcb9ca0bacdc1fd2f8744729c
Dilip9 <dilip.verma940805@gmail.com>
2025-12-09 01:39:03 +0530

    certification

--- src/main/java/gouri/aws/awscloud/r_service/CertificationService.java
+++ src/main/java/gouri/aws/awscloud/r_service/CertificationService.java
@@ -1,4 +1,12 @@
 package gouri.aws.awscloud.r_service;
 
+import gouri.aws.awscloud.model.dto.request.CertificationCreateRequest;
+import gouri.aws.awscloud.model.dto.response.CertificationDTO;
+import gouri.aws.awscloud.model.dto.update.CertificationUpdateRequest;
+
 public interface CertificationService {
+    CertificationDTO add(Long resumeId, CertificationCreateRequest request);
+    CertificationDTO update(Long id, CertificationUpdateRequest request);
+    void delete(Long id);
 }
+
