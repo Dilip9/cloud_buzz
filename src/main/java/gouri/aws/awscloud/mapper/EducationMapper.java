e42775adadeb05cbc3ee49967b81c9a56929b159
Dilip9 <dilip.verma940805@gmail.com>
2025-12-08 23:43:40 +0530

    Education Mapper Interface

--- src/main/java/gouri/aws/awscloud/mapper/EducationMapper.java
+++ src/main/java/gouri/aws/awscloud/mapper/EducationMapper.java
@@ -1,4 +1,15 @@
 package gouri.aws.awscloud.mapper;
 
-public class EducationMapper {
+
+@Mapper(componentModel = "spring")
+public interface EducationMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "resume", ignore = true)
+    Education toEntity(EducationCreateRequest request);
+
+    EducationDTO toDTO(Education education);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEducation(EducationUpdateRequest request, @MappingTarget Education entity);
 }
