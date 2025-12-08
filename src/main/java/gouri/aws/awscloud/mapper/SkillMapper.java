191e79985df34e223ab7376f023eddee71ba67bc
Dilip9 <dilip.verma940805@gmail.com>
2025-12-08 23:44:22 +0530

    Skill Mapper Interface

--- src/main/java/gouri/aws/awscloud/mapper/SkillMapper.java
+++ src/main/java/gouri/aws/awscloud/mapper/SkillMapper.java
@@ -1,4 +1,16 @@
 package gouri.aws.awscloud.mapper;
 
-public class SkillMapper {
+
+@Mapper(componentModel = "spring")
+public interface SkillMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "resume", ignore = true)
+    Skill toEntity(SkillCreateRequest request);
+
+    SkillDTO toDTO(Skill skill);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateSkill(SkillUpdateRequest request, @MappingTarget Skill entity);
 }
+
