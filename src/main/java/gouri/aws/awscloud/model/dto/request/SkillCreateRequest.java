71105fb158f021ddab14599fdab89460440b8922
Dilip9 <dilip.verma940805@gmail.com>
2025-12-11 23:27:58 +0530

    Skill Swagger update

--- src/main/java/gouri/aws/awscloud/model/dto/request/SkillCreateRequest.java
+++ src/main/java/gouri/aws/awscloud/model/dto/request/SkillCreateRequest.java
@@ -1,5 +1,6 @@
 package gouri.aws.awscloud.model.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import jakarta.validation.constraints.*;
 
@@ -7,9 +8,11 @@ import jakarta.validation.constraints.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Request DTO for creating a new skill")
 public class SkillCreateRequest {
 
     @NotBlank(message = "Skill name is required")
+    @Schema(description = "Name of the skill", example = "Java Programming")
     private String name;
 
     @NotBlank(message = "Skill level is required")
@@ -17,6 +20,7 @@ public class SkillCreateRequest {
             regexp = "Beginner|Intermediate|Advanced|Expert",
             message = "Skill level must be Beginner, Intermediate, Advanced, or Expert"
     )
+    @Schema(description = "Proficiency level of the skill", example = "Intermediate")
     private String level;
 }
 
--- src/main/java/gouri/aws/awscloud/model/dto/response/SkillDTO.java
+++ src/main/java/gouri/aws/awscloud/model/dto/response/SkillDTO.java
@@ -1,15 +1,20 @@
 package gouri.aws.awscloud.model.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "Data Transfer Object for Skill information")
 public class SkillDTO {
 
+    @Schema(description = "Unique identifier of the Skill", example = "1")
     private Long id;
 
+    @Schema(description = "Name of the Skill", example = "Java Programming")
     private String name;
+    @Schema(description = "Proficiency level of the Skill", example = "Expert")
     private String level;
 }
